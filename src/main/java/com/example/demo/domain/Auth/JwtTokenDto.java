ba5d13556247b0a7c7ceb28e5aa9b1b24badbcdd
Ryo <naberyo34@gmail.com>
2025-03-16 17:24:10 +0900

    fix: Auth関連ファイルを整理

---
+++ src/main/java/com/example/demo/domain/Auth/JwtTokenDto.java
@@ -0,0 +1,20 @@
+package com.example.demo.domain.Auth;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public class JwtTokenDto {
+  @Schema(description = "JWT トークン", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c")
+  private String token;
+
+  public JwtTokenDto(String token) {
+    this.token = token;
+  }
+
+  public String getToken() {
+    return token;
+  }
+
+  public void setToken(String token) {
+    this.token = token;
+  }
+}
