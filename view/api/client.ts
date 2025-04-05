b9e8272e176f6588d8a41729fda1f9ee4e22070f
Ryo <naberyo34@gmail.com>
2025-04-05 10:50:21 +0900

    chore

--- view/api/client.ts
+++ view/api/client.ts
@@ -1,4 +1,4 @@
 import createClient from "openapi-fetch";
 import type { paths } from "./schema";
 
-export const glitterApiClient = createClient<paths>({ baseUrl: process.env.GLITTER_API_URL });
+export const glitterApiClient = createClient<paths>({ baseUrl: process.env.API_URL });
